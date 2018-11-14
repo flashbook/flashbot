5441737b9bf50303c367db0b7d4daf02e120bb56
Alex Lopatin <aleksander.lopatin@gmail.com>
2018-11-13 21:55:31 -0600

    Json fixes

--- client/src/main/scala/io/flashbook/flashbot/ReportView.scala
+++ client/src/main/scala/io/flashbook/flashbot/ReportView.scala
@@ -1,6 +1,10 @@
 package io.flashbook.flashbot
 
+import io.circe.Printer
+import io.flashbook.flashbot.BacktestParams.StratParams
+import io.flashbook.flashbot.BacktestParams._
 import org.scalajs.dom
+import org.scalajs.dom.{Event, WebSocket}
 import slinky.core._
 import slinky.core.annotations.react
 import slinky.web.html.{className, div}
@@ -9,10 +13,16 @@ import slinky.web.html.{className, div}
 @react class ReportView extends StatelessComponent {
   case class Props(name: String)
 
-  var ws: Option[BacktestWebSocket] = None
-
   override def componentDidMount() = {
-    ws = Some(new BacktestWebSocket(s"ws://${dom.window.location.host}/backtest/${BacktestConfig.strategy}/ws"))
+
+    val ws = new WebSocket(s"ws://${dom.window.location.host}/backtest/${BacktestConfig.strategy}/ws")
+    ws.onopen = (e: Event) => {
+      val msg: String = paramsEn(???).pretty(Printer.noSpaces)
+      ws.send(msg)
+    }
+    ws.onmessage = msg => {
+      println(msg)
+    }
   }
 
   def render() = div(className := "Report tile is-ancestor")(
